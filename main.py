1d9f13dbd57066f23ef5e4a0cbfa9c5d1b3b562e
Sarbjot Singh <sarbdevs611@gmail.com>
2026-08-03 16:24:24 +0530

    Fix bugs and update project

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ from src.summary import generate_executive_summary
 
 
 def main():
-    # -----------------------------
     # Load & Clean Data
-    # -----------------------------
     file_path = "data/amazon_reviews.csv"
 
     df = load_data(file_path)
@@ -19,9 +17,7 @@ def main():
     df = clean_data(df)
     preview_data(df)
 
-    # -----------------------------
     # AI Classification
-    # -----------------------------
     batch_size = 10
 
     print("\nStarting batch classification...\n")
@@ -42,30 +38,22 @@ def main():
     df["sentiment"] = [result["sentiment"] for result in all_results]
     df["topic"] = [result["topic"] for result in all_results]
 
-    # -----------------------------
     # Save Classified Dataset
-    # -----------------------------
     output_file = "data/classified_reviews.csv"
 
     df.to_csv(output_file, index=False)
 
     print(f"\n✅ Classified dataset saved to: {output_file}")
 
-    # -----------------------------
     # Analysis
-    # -----------------------------
     print("\nGenerating analysis...")
     analyze_data()
 
-    # -----------------------------
     # Dashboard
-    # -----------------------------
     print("\nGenerating dashboard...")
     create_dashboard()
 
-    # -----------------------------
     # Executive Summary
-    # -----------------------------
     print("\nGenerating executive summary...")
     generate_executive_summary()
 
